da22f2b06fc0a9cf554f203ce0b0d48677ca9b47
Joko Mulyono <jokomulyono696@gmail.com>
2017-11-24 11:01:55 +0700

    modify

--- www/js/controller/yoride_ctrl.js
+++ www/js/controller/yoride_ctrl.js
@@ -13,7 +13,7 @@ app.controller('yorideCtrl', function($scope, $state, $cordovaGeolocation, $ioni
     var backdrop = document.getElementsByClassName('backdrop');
     angular.element(backdrop).attr('data-tap-disabled', 'true');
     angular.element(container).on("click", function() {
-      document.getElementById('search').blur();
+      document.getElementById('pac-input').blur();
     });
   };
 
@@ -147,8 +147,8 @@ app.controller('yorideCtrl', function($scope, $state, $cordovaGeolocation, $ioni
     var search = document.getElementById('search');
     var inputtuj = document.getElementById('tuj-input');
     //var searchDari = new google.maps.places.SearchBox(inpurdari);
-    // var searchBox = new google.maps.places.SearchBox(input);
-    // var searchBoxtuj = new google.maps.places.SearchBox(inputtuj);
+    var searchBox = new google.maps.places.SearchBox(input);
+    var searchBoxtuj = new google.maps.places.SearchBox(inputtuj);
 
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
     $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(search);
